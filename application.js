88f22160121d449cad818787aebe267f2302daf4
tangzifan <tangzifan@didiapp.com>
2021-01-21 10:13:51 +0800

    commit

--- application.js
+++ application.js
@@ -80,7 +80,7 @@ System.register([], function (_export, _context) {
 
     function loadSettingsJson(cc) {
       return new Promise(function (resolve, reject) {
-        cc.loader.load('./res/settings.61010.json', function (err, json) {
+        cc.loader.load('./res/settings.6f368.json', function (err, json) {
           if (err) {
             return reject(err);
           }
