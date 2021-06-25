60a391278e7cd80d485cfd783d5b954d7c4a5eae
tangzifan <tangzifan@didiapp.com>
2021-06-25 14:52:24 +0800

    commit

---
+++ cocos3d-js/cc.wait-for-ammo-instantiation.js
@@ -0,0 +1,13 @@
+System.register(['./ammo-instantiated-e4ebeae9.js'], function (exports) {
+	'use strict';
+	return {
+		setters: [function (module) {
+			exports('default', module.w);
+		}],
+		execute: function () {
+
+
+
+		}
+	};
+});
